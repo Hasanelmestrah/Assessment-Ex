33d106cc8e7c099a2653be198ce39ad82f3c3886
hasanelmestrah <hasanelmestrah@gmail.com>
2023-02-27 12:13:08 +0200

    test

--- server.js
+++ server.js
@@ -1,18 +1,63 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "/config/db.js";
-dotenv.config();
-connectDB();
-
-const PORT = process.env.PORT || 5000;
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
-app.use(errorHandler);
+app.use(express.json());
+app.use(cors());
 
-app.use("/api/project", testRoutes);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/react-todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(console.error);
 
-app.listen(
-  PORT,
-  console.log(`Server is running in ${process.env.NODE_ENV} on port ${PORT}!!!`)
-);
+// Models
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos);
+});
+
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.delete("/todo/delete/:id", async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id);
+
+  res.json({ result });
+});
+
+app.get("/todo/complete/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.complete = !todo.complete;
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.put("/todo/update/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.text = req.body.text;
+
+  todo.save();
+
+  res.json(todo);
+});
+
+app.listen(3001);
